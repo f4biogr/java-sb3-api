c6f7a83cb85e7bffe3eba35a3a62d9d4a1ac5813
Fábio da Rosa <fabio.gorosa@gmail.com>
2023-06-13 20:18:21 -0300

    :recycle: Refactor ProductRepository by removing unnecessary annotations

--- src/main/java/com/example/api/repositories/ProductRepository.java
+++ src/main/java/com/example/api/repositories/ProductRepository.java
@@ -2,9 +2,8 @@ package com.example.api.repositories;
 
 import com.example.api.models.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-@Repository
+
 public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
 }
