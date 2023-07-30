7216a3e1a524266243dd9cca1e7390b7f9632b49
Fábio da Rosa <fabio.gorosa@gmail.com>
2023-07-30 03:02:51 -0300

    :fire: Remove unnecessary line break in ProductRepository interface

--- src/main/java/com/example/api/repositories/ProductRepository.java
+++ src/main/java/com/example/api/repositories/ProductRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
-}
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {}
