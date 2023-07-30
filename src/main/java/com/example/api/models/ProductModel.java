7a188188cae1e2b25c339359aa30728ceb5ce9c5
Fábio da Rosa <fabio.gorosa@gmail.com>
2023-07-30 03:00:28 -0300

    :recycle: Refactor `ProductModel` class

--- src/main/java/com/example/api/models/ProductModel.java
+++ src/main/java/com/example/api/models/ProductModel.java
@@ -9,6 +9,7 @@ import lombok.Setter;
 import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Objects;
 import java.util.UUID;
 
 import org.springframework.hateoas.RepresentationModel;
@@ -20,12 +21,27 @@ import org.springframework.hateoas.RepresentationModel;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 1L;
+  @Serial private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
-    private String name;
-    private BigDecimal price;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  private String name;
+  private BigDecimal price;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof ProductModel that)) return false;
+    if (!super.equals(o)) return false;
+    return Objects.equals(getId(), that.getId())
+        && Objects.equals(getName(), that.getName())
+        && Objects.equals(getPrice(), that.getPrice());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(super.hashCode(), getId(), getName(), getPrice());
+  }
 }
